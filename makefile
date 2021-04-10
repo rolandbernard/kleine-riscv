cfae58207973bbfb89bad2bc9c4925a9efe5275a
Roland Bernard <rolli.bernard@gmail.com>
2021-04-10 20:11:13 +0200

    Added a core simulator

--- makefile
+++ makefile
@@ -11,7 +11,8 @@ UNITS_DIR := $(TEST_DIR)/units
 UNIT_SRC   := $(shell find $(UNITS_DIR) -type f -name '*.cpp')
 UNIT_TESTS := $(patsubst $(UNITS_DIR)/%.cpp, $(BUILD_DIR)/V%, $(UNIT_SRC))
 ISA_SRC    := $(shell find $(ISA_DIR) -type f -name '*.S')
-ISA_TESTS  := $(patsubst $(ISA_DIR)/%.S, $(ISA_DIR)/build/%.bin, $(ISA_SRC))
+ISA_BIN    := $(patsubst $(ISA_DIR)/%.S, $(ISA_DIR)/build/%.bin, $(ISA_SRC))
+ISA_TESTS  := $(ISA_BIN:.bin=.hex)
 # ==
 
 # == Runing goals
@@ -26,7 +27,7 @@ VERIFLAGS := $(addprefix -I,$(shell find $(SRC_DIR) -type d)) -Wall -Mdir $(BUIL
 .SILENT:
 .SECONDARY:
 .SECONDEXPANSION:
-.PHONY: test build-tests RUNTEST.$(BIN)/%
+.PHONY: test build-tests RUNTEST.$(BUILD_DIR)/% RUNTEST.$(ISA_DIR)/build/%
 
 test: $(RUNTESTS)
 
@@ -34,13 +35,25 @@ $(BUILD_DIR)/V%: $(SRC_DIR)/units/%.v $(UNITS_DIR)/%.cpp | $$(dir $$@)
 	@echo Building $@
 	$(VERILATOR) $(VERIFLAGS) --cc --exe --build $^
 
+$(BUILD_DIR)/V%: $(SRC_DIR)/%.v $(TEST_DIR)/core/%.cpp | $$(dir $$@)
+	@echo Building $@
+	$(VERILATOR) $(VERIFLAGS) --cc --exe --build $^
+
 %/:
 	mkdir -p $@
 
-$(ITEST)/build/%.bin: $(ITEST)/build/%.bin
+$(ISA_DIR)/build/%.bin: $(ISA_SRC)
 	$(MAKE) -C tests/isa
+
+$(ISA_DIR)/build/%.hex: $(ISA_DIR)/build/%.bin
+	@echo Building $@
+	hexdump -e '4/4 "%08x " "\n"' $< > $@
 
 RUNTEST.$(BUILD_DIR)/%: $(BUILD_DIR)/%
 	@echo "Running test $(notdir $<)"
 	$<
+
+RUNTEST.$(ISA_DIR)/build/%: $(ISA_DIR)/build/% | $(BUILD_DIR)/Vcore
+	@echo "Running test $(notdir $<)"
+	$(BUILD_DIR)/Vcore $<
 
--- tests/core/core.cpp
+++ tests/core/core.cpp
@@ -1,10 +1,88 @@
 
+#include <fstream>
 #include <iostream>
+#include <stdint.h>
+#include <stdio.h>
+#include <string>
+#include <unordered_map>
+
+#include "Vcore.h"
+
+struct MagicMemory {
+    std::unordered_map<uint32_t, uint32_t> data;
+
+    void handleRequest(Vcore &core) {
+        if (core.ext_valid) {
+            if (core.ext_write_strobe == 0) {
+                if (data.find(core.ext_address) != data.end()) {
+                    core.ext_read_data = data[core.ext_address];
+                } else {
+                    core.ext_read_data = 0;
+                }
+            } else {
+                if (0x10000000 & core.ext_address) {
+                    exit(core.ext_write_data);
+                } else {
+                    uint32_t new_data = data[core.ext_address];
+                    for (int i = 0; i < 4; i++) {
+                        if (core.ext_write_strobe & (1 << i)) {
+                            new_data &= ~(0xff << (8 * i));
+                            new_data |= (0xff << (8 * i)) & core.ext_write_data;
+                        }
+                    }
+                    data[core.ext_address] = new_data;
+                }
+            }
+            core.ext_ready = true;
+        } else {
+            core.ext_ready = false;
+        }
+    }
+
+    void delayRequest(Vcore &core) {
+        core.ext_ready = false;
+    }
+
+    bool loadFromFile(std::string filename) {
+        std::ifstream file(filename);
+        if (file.is_open()) {
+            uint32_t value;
+            for (uint32_t i = 0; file >> std::hex >> value; i += 4) {
+                data[i] = value;
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
 
 int main(int argc, char** argv) {
     if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " [FILE]" << std::endl;
     } else {
+        MagicMemory memory;
+        Vcore core;
+        memory.loadFromFile(std::string(argv[1]));
+        core.reset = 1;
+        core.eval();
+        core.clk = 0;
+        core.eval();
+        core.clk = 1;
+        core.eval();
+        core.clk = 0;
+        core.eval();
+        core.reset = 0;
+        core.eval();
+        memory.handleRequest(core);
+        for (;;) {
+            core.eval();
+            core.clk = 1;
+            core.eval();
+            core.clk = 0;
+            core.eval();
+            memory.handleRequest(core);
+        }
     }
 }
 
